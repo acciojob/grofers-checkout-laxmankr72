32114edec468fbf2aa92d7d59f771cf240e60ab7
Laxman Kumar <80440232+laxmankr72@users.noreply.github.com>
2026-01-18 21:13:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,31 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
+
+    const prices = document.querySelectorAll(".price");
+
+    let total = 0;
+
+   
+    prices.forEach(price => {
+        total += Number(price.innerText);
+    });
+
+
+    const table = document.querySelector("table");
+
+    const tr = document.createElement("tr");
+	tr.innerHTML ="<td colspan=2></td>";
+	tr.innerText = `Total Price: ${total}`;
+	
+	
+    // const td = document.createElement("td");
+
+    // td.setAttribute("colspan", "2");
+    // td.innerText = `Total Price: ${total}`;
+
+    // tr.appendChild(td);
+    table.appendChild(tr);
   
 };
 
