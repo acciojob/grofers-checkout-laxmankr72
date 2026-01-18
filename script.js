5200cd91d35ac1b9f2fe05b55abe2e9b88a6c9c7
Laxman Kumar <80440232+laxmankr72@users.noreply.github.com>
2026-01-18 21:26:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,32 +4,24 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 
-    const price = document.querySelectorAll(".prices");
-
+    const prices = document.querySelectorAll(".prices");
     let total = 0;
 
-   
-    price.forEach(price => {
+    prices.forEach(price => {
         total += Number(price.innerText);
     });
-
 
     const table = document.querySelector("table");
 
     const tr = document.createElement("tr");
-	tr.innerHTML ="<td colspan=2></td>";
-	tr.innerText = `Total Price: ${total}`;
-	
-	
-    // const td = document.createElement("td");
+    const td = document.createElement("td");
 
-    // td.setAttribute("colspan", "2");
-    // td.innerText = `Total Price: ${total}`;
+    td.setAttribute("colspan", "2");
+    td.setAttribute("id", "ans"); 
+    td.innerText = total;         
 
-    // tr.appendChild(td);
+    tr.appendChild(td);
     table.appendChild(tr);
-  
 };
 
 getSumBtn.addEventListener("click", getSum);
-
